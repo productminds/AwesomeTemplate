333db534d4fb6e029b89cfba9b1dd90a86150851
Antonny Santos <antonnysantos@MacBook-Pro-de-Antonny.local>
2024-07-04 18:42:15 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -25,12 +25,15 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Braze from '@braze/react-native-sdk';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -61,6 +64,10 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    Braze.changeUser('testing4');
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -2,6 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <BrazeKit/BrazeKit-Swift.h>
+#import "BrazeReactBridge.h"
+
+static Braze *_braze = nil;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -11,7 +16,26 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+   // Setup Braze
+  BRZConfiguration *configuration = [[BRZConfiguration alloc] initWithApiKey:@"d781e48b-40b3-4544-9cdc-fff77859d0e7"
+                                                                    endpoint:@"sdk.iad-06.braze.com"];
+  // Enable logging and customize the configuration here.
+  configuration.logger.level = BRZLoggerLevelDebug;
+  Braze *braze = [BrazeReactBridge initBraze:configuration];
+  AppDelegate.braze = braze;
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+#pragma mark - AppDelegate.braze
+
+
++ (Braze *)braze {
+  return _braze;
+}
+
++ (void)setBraze:(Braze *)braze {
+  _braze = braze;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
